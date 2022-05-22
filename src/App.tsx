b6bcdce5765c9c7953d937390baf29543b944738
philstronder <felipevale@gmail.com>
2022-05-22 14:26:27 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,83 @@
+import React from 'react'
+import { useAppDispatch, useAppSelector } from './app/hooks'
+import { incremented, amountAdded } from './features/counter/counter-slice'
+import { useFetchBreedsQuery } from './features/dogs/dogs-api-slice'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  const count = useAppSelector(state => state.counter.value);
+  const dispatch = useAppDispatch();
+
+  const { data = [], isFetching } = useFetchBreedsQuery();
+
+  const handleClick = () => {
+    //increment by 1
+    // dispatch(incremented());
+
+    //increment by a fixed amount
+    dispatch(amountAdded(3));
+  }
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Vite + React!</p>
+        <p>
+          <button type="button" onClick={handleClick}>
+            count is: {count}
+          </button>
+        </p>
+
+        <div>
+          <p>Number of dogs fetched: {data.length}</p>
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Picture</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((breed) => (
+                <tr key={breed.id}>
+                  <td>{breed.name}</td>
+                  <td>
+                    <img src={breed.image.url} alt={breed.name} height={250} />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
